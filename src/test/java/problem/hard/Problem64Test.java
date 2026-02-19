7da8495eca7afc49d35c354f3c79b2f32ac1c745
toctoce <toctoce@naver.com>
2026-02-19 21:43:26 +0900

    fix : hard 64 수정

--- src/test/java/problem/hard/Problem64Test.java
+++ src/test/java/problem/hard/Problem64Test.java
@@ -9,6 +9,6 @@ public class Problem64Test {
 
     @Test
     public void testSumOfFibonacciNumbers() {
-        assertEquals(19, Problem64.sumOfFibonacciNumbers(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        assertEquals(19, Problem64.sumOfFibonacciNumbers(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8)));
     }
 }
