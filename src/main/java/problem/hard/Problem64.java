9d454f84c11b61322f32bba8b65a97da81a6deed
toctoce <toctoce@naver.com>
2026-02-19 21:43:34 +0900

    solve: hard 64

--- src/main/java/problem/hard/Problem64.java
+++ src/main/java/problem/hard/Problem64.java
@@ -5,14 +5,25 @@ import java.util.List;
 public class Problem64 {
 
     /**
-     * 주어진 정수 리스트에서 피보나치 수열에 해당하는 수들의 합을 계산합니다.
-     * 피보나치 수열은 0, 1, 1, 2, 3, 5, 8, 13, ... 과 같이 이전 두 수의 합으로 이루어진 수열입니다.
+     * 주어진 정수 리스트에서 피보나치 수열에 해당하는 수들의 합을 계산합니다. 피보나치 수열은 0, 1, 1, 2, 3, 5, 8, 13, ... 과 같이 이전 두 수의 합으로 이루어진 수열입니다.
      *
      * @param numbers 정수 리스트
      * @return 피보나치 수열에 속하는 수들의 합
      */
     public static int sumOfFibonacciNumbers(List<Integer> numbers) {
         // 여기에 코드 작성
-        return 0;
+        return numbers.stream()
+                .filter(num -> {
+                    int num1 = 0;
+                    int num2 = 1;
+                    while (num1 < num) {
+                        int tmp = num1 + num2;
+                        num1 = num2;
+                        num2 = tmp;
+                    }
+                    return num1 == num;
+                })
+                .mapToInt(num -> num)
+                .sum();
     }
 }
